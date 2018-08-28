022d456ede918c39d0230b15031f846f0aae0ae7
71ck5 <nicholas@nicholasjeske.com>
2018-08-28 02:03:57 -0500

    V1.1
    1.allow for in-line information fill in. 2.allow for selecting of car and auto select if only one 3.allow for google address smtp to be auto resolved. 4.Fixed poor naming of file. 5.Updated Readme.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Python-Tesla-Plugin-Reminder
+
+You are going to need to modify the file to your needs if you don't want to have to re-enter it everytime, such as:
+
+ 1. Lat and long for an area
+ 2. Vehicle id
+ 3. Tesla email and pass
+ 4. email email and pass
+ 5. email smtp server and port
+
+**Tip**
+If you have at&t you can have it text you at {phone number}@txt.att.net
+
+**Todo**
+- [X] Allow for picking of vehicle or default to only vehicle if one
+- [X] Add Tesla email and pass question in line
+- [X] Add email and pass question in line
+- [X] If certain adress then know the smtp and port
+- [ ] Larger smtp Autoresolve database
--- Tesla_Charging.py
+++ Tesla_Charging.py
@@ -4,6 +4,8 @@ import urllib.parse
 import smtplib
 import json
 import time
+import getpass
+a = 0
 
 #min and max so -87 is less then -97 and 6 greater then 2.
 min_lat="" #minimum lat; FILL
@@ -14,15 +16,15 @@ base_url="https://owner-api.teslamotors.com"
 api_url=(base_url + "/api/1")
 CLIENT_ID = "81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384"
 CLIENT_SECRET = "c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3"
-vehicle_id="" #this is not Vehicle_id or id but id_s; FILL
-t_email="" #tesla email; FILL
-t_password="" #tesla password ; FILL
-e_email="" # email email; FILL
-e_password="" # email password; FILL
+vehicle_id=[] #this is not Vehicle_id or id but id_s; FILL
+t_email=input('Please input Telsa login email\n') #tesla email; FILL
+t_password=getpass.getpass('Please input Telsa login password\n') #tesla password ; FILL
+e_email=input('Please input email login email\n') # email email; FILL
+e_password=getpass.getpass('Please input email login password\n') # email password; FILL
 e_time="3600" #time inbetween checks Default=1HR
 smtp_server="" # smtp sever Example: smtp.gmail.com; FILL
 smtp_port="" # smtp port Example: 587; FILL
-toaddr = e_email # address send msg Default: e_email or your email 
+toaddr=input('Please input destination email for notification\n') # address to send msg to
 
 auth_para = {
             "grant_type": "password",
@@ -32,10 +34,31 @@ auth_para = {
             "password": t_password
         }
 
+a = 0 # for increment
 auth_param = urllib.parse.urlencode(auth_para)#this is for encoding to allow for use in urlopen
 auth_param = auth_param.encode('ascii')       #^ 
 auth = json.load(urllib.request.urlopen(base_url + "/oauth/token", data=auth_param))
 headers = {'Authorization': 'Bearer {a}'.format(a=(auth["access_token"]))} # assign access token
+
+car = urllib.request.Request(api_url + '/vehicles', headers=headers) #car selection
+ca = (json.load(urllib.request.urlopen(car)))
+if (ca['count'] == 1):
+    vehicle_id = ca['response'][0]['id_s']
+    print ("Only car selected")
+else:
+    for item in ca['response']:
+        a += 1
+        print (str(a) + '.' + item['display_name'])
+    re = int(input('\nPlease choose a vehicle by number from 1 to ' + str(a) + ' \nExample: 1\n\n')) - 1
+    vehicle_id = ca['response'][re]['id_s']
+    print (vehicle_id)
+    
+if ("@google.com" in e_email) and (smtp_sever == smtp_port): #make it slightly easier
+    smtp_server="smtp.gmail.com"
+    smtp_port="587"
+else:
+    smtp_server=input('\nPlease input your smtp server \nExample: smtp.gmail.com\n\n')
+    smtp_port=int(input('\nPlease input your smtp server \nExample: 587\n\n'))
 
 def req(req_name): #For json api calls
     reqe = urllib.request.Request(api_url + '/vehicles/{vehicle_id}/data_request/{req_name}'.format(vehicle_id=vehicle_id,req_name=req_name), headers=headers)
